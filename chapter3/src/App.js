5c3d5f3128ebaf47318d2646820c211acd93bcd1
Kuroneko <ken.aster8@gmail.com>
2024-04-02 10:14:09 +0900

    途中まで

--- chapter3/src/App.js
+++ chapter3/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const CounterText = (props) => <p> now count is {props.count}</p>;
+const Counter = () => {
+  const INITIAL_COUNT = 0;
+  const [count, setCount] = useState(INITIAL_COUNT);
+
+  const countAdd = () => setCount((prevCount) => prevCount + 1);
+  const countSub = () => setCount((prevCount) => prevCount - 1);
+  const countReset = () => setCount(INITIAL_COUNT);
+
+  return (
+    <div>
+      <button onClick={countAdd}>Button + 1</button>
+      <button onClick={countSub}>Button - 1</button>
+      <button onClick={countReset}>Reset</button>
+      <CounterText count={count} />
+    </div>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter></Counter>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,5 +42,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
